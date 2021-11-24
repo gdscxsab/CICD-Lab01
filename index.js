4aa1a66c28fedc001d92c8f9ba6a24381ebc3af7
Tran Minh Triet <tmtriet00@gmail.com>
2021-11-24 13:50:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to CI/CD Exercise of CS101");
+});
+
+app.listen(port, () => {
+  console.log("Server is listening");
+});
+module.exports = app;
